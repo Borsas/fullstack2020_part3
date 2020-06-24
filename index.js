2419c355b58ec89f116d8809f7d216f9cb6c21f0
Borsas <borsasyt@gmail.com>
2020-06-24 21:54:49 +0300

    3.3 done

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const notes = {
-    "persons": [
+const persons = [
       {
         "name": "Dan Abramov",
         "number": "143251346134613461346",
@@ -15,17 +14,26 @@ const notes = {
         "number": "39-23-6423122",
         "id": 4
       }
-    ]
-  }
+  ]
 
-app.get("/api/notes", (req, resp) => {
-    resp.json(notes)
+app.get("/api/persons", (req, resp) => {
+    resp.json(persons)
+})
+
+app.get("/api/persons/:id", (req, resp) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        resp.json(person)
+    } else {
+        resp.status(404).end()
+    }
 })
 
 app.get("/info", (req, resp) => {
     const date = new Date()
     resp.send(`
-    <div>Phonebook has info for ${notes.persons.length} people</div>
+    <div>Phonebook has info for ${persons.length} people</div>
     <br>
     <div>${date}</div>
     `)
